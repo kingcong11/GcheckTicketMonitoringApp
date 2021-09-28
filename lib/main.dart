256737c9d2ff0c6209b525ea865ab5310e740822
Kingcong11 <nico.ativo@bicol-u.edu.ph>
2021-09-28 11:19:11 +0800

    new changes

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'This is now the login page',
             ),
             Text(
               '$_counter',

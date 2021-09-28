48a8c5d0ab7be4b700910a794ff069af871222f7
Kingcong11 <nico.ativo@bicol-u.edu.ph>
2021-09-28 14:36:34 +0800

    test

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'This is now the login dashboard',
+              'Hi Developers please inherit all my new updates',
             ),
             Text(
               '$_counter',
